2ff1be66e03c6e10b7735dd3165470f2fe720fd2
Jesantox <Jesantoxfachmann@gmail.com>
2021-04-17 04:44:48 -0300

    aula 01 - Função Console
    Signed-off-by: Jesantox <Jesantoxfachmann@gmail.com>

---
+++ aula_01/FuncaoConsole.js
@@ -0,0 +1,14 @@
+
+// Quebra linha;
+// Função Exibir > Similar ao print em Java;
+console.log('Hello World!'); 
+
+// Exibindo Strings (Textos) - Manipulando Aspas e Crase
+console.log(" 'Jean' `Santos`"); // Aspas Simples e Crase dentro de Aspas Dupla
+console.log(' "Jean" `Santos`'); // Aspas Dupla e Crase dentro de Aspas Simples
+console.log(` 'Jean' "Santos"`); // Aspas Simples e Aspas Dupla dentro de Crase
+
+// Exibindo Números (Numbers)
+// Não existe diferenciação de números Inteiros e Flutuantes 
+// > Tudo é do tipo Number
+console.log(17, 20.50, "Jean Santos")
