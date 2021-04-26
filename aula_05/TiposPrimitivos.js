16368730f796f71e54547fe01d8d9851789a0391
Jesantox <Jesantoxfachmann@gmail.com>
2021-04-26 02:39:54 -0300

    Tipos Primitivos
    Signed-off-by: Jesantox <Jesantoxfachmann@gmail.com>

---
+++ aula_05/TiposPrimitivos.js
@@ -0,0 +1,24 @@
+// DADOS PRIMITIVOS  
+// STRING, NUMBER, UNDEFINED, NULL, BOOLEAN, SYMBOL
+
+const nome0 = "Jean"; // String
+const nome1 = 'Jean'; // String
+const nome2 = `Jean`; // String -> Muito usado em Templates
+
+const numero0 = 17;   // number -> Inteiro
+const numero1 = 17.50 // number -> Float
+
+let valorIndefinido; // undefined
+let nulo = null; 
+// -> O null serve para vc transformar o valor de algo em nulo
+// -> Muito usado em Estruturas Condicionais.
+// -> Por exemplo, Caso o usuário nao escolha uma 'cor' o valor é 'null"
+//    Após ele escolher, o valor null passa a ser 'red', por exemplo.
+
+const aprovado = true; // valor boolean
+const reprovado = false; // Valor boolean
+
+// vendo o tipo e o valor de uma variável
+console.log(typeof nome0, nome0);
+
+
