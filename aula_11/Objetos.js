0db13ec9047b0ca976136f4b673ef3756fe3765a
Jesantox <Jesantoxfachmann@gmail.com>
2021-05-30 01:54:00 -0300

    aula_11 - Melhorias e Correções
    Signed-off-by: Jesantox <Jesantoxfachmann@gmail.com>

--- aula_11/Objetos.js
+++ aula_11/Objetos.js
@@ -1,14 +1,29 @@
-// Objetos servem pra agrupar atributos 
-// Objetos são criados com chaves '{}'
-// Arrays são criados com Colchetes '[]'
-// Atributos são separados por vírgula ','
-// Valores dos atributos são atribuídos por dois pontos ':'
+/*
+  ------------------------------------------------------------------------------
+  | Coleção de Propriedades | Objeto |
+  ------------------------------------------------------------------------------
 
-/* 
-   Sobre o 'this' 
-   > Pega o valor de um atributo 
-   >> que está dentro de um Objeto
-   >>> e usa dentro de uma função
+  ------------------------------------------------------------------------------
+  | ANOTAÇÕES |
+  ------------------------------------------------------------------------------
+ 
+  > Objetos servem pra agrupar atributos 
+  > Objetos são criados com chaves '{}'
+  > Arrays são criados com Colchetes '[]'
+  > Atributos são separados por vírgula ','
+  > Valores dos atributos são atribuídos por dois pontos ':'
+  
+  > Não precisa repetir os atributos ( Ex -> nome : nome).
+    JS entende que o atributo 'nome' é referente ao parâmetro 'nome'
+ 
+  ------------------------------------------------------------------------------
+  | SOBRE O THIS |
+  ------------------------------------------------------------------------------ 
+  
+  Serve para pegar o valor de um atributo, que está dentro de uma variável,
+  ou seja, um Objeto. Para assim, poder usarmos esse valor dentro de uma Função,
+  que também está dentro do mesmo Objeto.
+
 */
 
 // ----------------------------1º Forma de Criação------------------------------
@@ -29,24 +44,10 @@ console.log(pessoa.idade);
 
 // -----------------2º Forma de Criação - Mais correta e Flexível---------------
 
-/*  Função que cria Objetos
-
-    function criarPessoa (nome, sobrenome, idade -> Parâmetros){
-    return{
-      nome : nome,
-      sobrenome : sobrenome,
-      idade : idade
-    };
-  }
-
-  Não precisa repetir (nome : nome)
-*/
-
+// Função que cria Objetos
 function criarPessoa(nome, sobrenome, idade /* Parâmetros */) {
   return { nome, sobrenome, idade };
-}
-
-// a linguagem ja entende que o atributo 'nome' é referente ao parâmetro 'nome' 
+} 
 
 const str = '-';
 const pessoa01 = criarPessoa('Roberta', 'Valena', 40 /* Argumentos */);
@@ -72,12 +73,12 @@ const aluno = {
   sobrenome: 'Santos',
   idade: 17,
 
-  // 1º Função
+  // 1º Método - Usando this para pegar o valor do Atributo
   fala() {
     console.log(`${this.nome} ${this.sobrenome} tem ${this.idade}`);
   },
 
-  // 2º Função
+  // 2º Método
   aumentarIdade() {
     this.idade++;
   }
